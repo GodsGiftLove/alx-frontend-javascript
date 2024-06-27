86ff811144819a4db7015368f5e0882b77e5b7ba
GodsGiftLove <emefak77@gmail.com>
2024-06-27 06:36:30 +0000

    it
    promise

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,5 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-    resolve('Example');
-    reject(new Error('Error message here'));
+  return new Promise((resolve) => {
+    resolve('true');
   });
 }
